9e75c37f13287be4d3710208be3583b59ec939bf
Pavel Kajaba <pkajaba@redhat.com>
2016-08-15 10:16:22 +0200

    Enable usage of httpd

---
+++ files/anitya.wsgi
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+import sys, os
+sys.path.insert (0,'/src/anitya/src')
+os.chdir('/src/anitya/')
+
+from anitya.app import APP as application
--- files/runincontainer.sh
+++ files/runincontainer.sh
@@ -3,5 +3,4 @@
 THISDIR=`dirname $0`
 
 source ${THISDIR}/anitya-env.sh
-
-python runserver.py --host '0.0.0.0'
+exec mod_wsgi-express start-server /src/anitya.wsgi --port 5000 --host 0.0.0.0 --user apache --group apache --access-log --access-log-format "%h %l %u %t \"%r\" %>s %b %{Referer}i \"%{User-agent}i\"" --log-to-terminal
