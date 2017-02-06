23c7c9c09eb2ac2af877ab87dbb443f09f55e7bc
Slavek Kabrda <bkabrda@redhat.com>
2017-02-06 10:50:32 +0100

    Add a downstream patch that allows matching artifacts by name and ecosystem rather than by name and homepage

--- files/runupdatejob.sh
+++ files/runupdatejob.sh
@@ -4,4 +4,4 @@ THISDIR=`dirname $0`
 
 source ${THISDIR}/anitya-env.sh
 export PYTHONPATH=${THISDIR}
-exec python ${THISDIR}/files/anitya_cron.py --check-feed
+exec python ${THISDIR}/files/anitya_cron.py --check-feed --by-ecosystem
