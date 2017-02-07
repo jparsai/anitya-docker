235df344d7842fa5e9351395fcdc787f8bd2bfc1
Slavek Kabrda <bkabrda@redhat.com>
2017-02-07 11:24:39 +0100

    Make the container job run continously in a single container

--- files/runupdatejob.sh
+++ files/runupdatejob.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
 THISDIR=`dirname $0`
 
 source ${THISDIR}/anitya-env.sh
 export PYTHONPATH=${THISDIR}
-exec python ${THISDIR}/files/anitya_cron.py --check-feed --by-ecosystem
+while :; do
+  python ${THISDIR}/files/anitya_cron.py --check-feed --by-ecosystem
+  sleep ${WAIT_SECONDS:-60}
+done
